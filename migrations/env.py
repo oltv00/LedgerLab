a82592b1a73636dfdb04e474e6006b01e2edeb17
oltv00 <oltv00@gmail.com>
2026-08-25 11:25:05 +0700

    set sqlaclhemy url

--- migrations/env.py
+++ migrations/env.py
@@ -1,3 +1,4 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -8,6 +9,8 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+database_url = os.environ['DATABASE_URL']
+config.set_main_option('sqlalchemy.url', database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
